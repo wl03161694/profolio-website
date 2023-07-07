3330c89adceb0e70572fbb1f55207161a48068d4
wl03161694 <ccy1834@gmail.com>
2023-07-08 00:43:14 +0800

    header and logo

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,12 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: 
+    {
+      fontFamily: {
+        primary: "Ade-display",
+      }
+    },
   },
   plugins: [],
 }
