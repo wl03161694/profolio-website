10c3845cdd91e0485193286792d21dee1055ef9a
wl03161694 <ccy1834@gmail.com>
2023-07-08 00:55:56 +0800

    add Ade font

--- src/App.vue
+++ src/App.vue
@@ -13,9 +13,9 @@
     </nav>
   </header>
   <div class="px-20 grid grid-cols-12 bg-[#F5EFE6] h-[100vh] w-[100vw]">
-    <h2 class="font-primary col-span-4 text-3xl font-bold">
+    <h2 class="font-primary col-span-4 text-3xl">
       HI,<br>
-      I'm Joey.
+      I'M JOEY.
     </h2>
     <div class="justify-center h-[100px] w-[100px] col-span-4 rounded-full bg-white"></div>
     <h2 class="col-span-4">
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     extend: 
     {
       fontFamily: {
-        primary: "Ade-display",
+        primary: "Ade-Display",
       }
     },
   },
